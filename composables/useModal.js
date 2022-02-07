bfc85e477faead9d9f8b954283c5018e75098dbc
igor <igor@flarehub.io>
2022-02-07 17:10:43 +0200

    Fix closing bug in useModal and add documentation

--- composables/useModal.js
+++ composables/useModal.js
@@ -1,8 +1,23 @@
-import { reactive, readonly } from "vue"
+import { reactive } from "vue"
 
+/**
+ * Reactive representation of the [modal properties]{@link https://grapesjs.com/docs/modules/Modal.html#customization}.
+ * @typedef ModalProps
+ * @property {Boolean} open Indicates if the modal should be open
+ * @property {Node} title Modal title
+ * @property {Node} content Modal content
+ * @property {Object} attributes Modal custom attributes
+ * @property {Function} close A callback to use when you want to close the modal
+ */
+
+/**
+ * Get object to handle the GrapesJS modal.
+ * @param {VGCconfig} grapes As provided by useGrapes
+ * @returns {ModalProps}
+ */
 export default function (grapes) {
   // Ensure GrapesJs is not yet initialised
-  if (grapes.initialized) throw new Error('useModalmust be executed before GrapesJs is initialised (onMount where useGrapes is executed)')
+  if (grapes.initialized) throw new Error('useModal must be executed before GrapesJs is initialised (onMount where useGrapes is executed)')
 
   // Take asset manager from cache if it already exists
   if (!grapes._cache.modal) {
@@ -21,12 +36,21 @@ export default function (grapes) {
       close() { },
     })
 
-    grapes.onInit((editor) => editor.on('modal', props => {
-      for (prop in props) {
-        modal[prop] = props[prop]
+    grapes.onInit((editor) => {
+      // Update modal handler when modal is triggered
+      editor.on('modal', props => {
+        for (const prop in props) {
+          if (prop !== 'close') modal[prop] = props[prop]
+        }
+      })
+
+      // Handle modal closure
+      modal.close = function () {
+        this.open = false
+        editor.Modal.close()
       }
-    }))
+    })
   }
 
-  return readonly(grapes._cache.modal)
+  return grapes._cache.modal
 }
